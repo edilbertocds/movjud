69a84a4242c2e44b6f9525167531c9120aee837c
Edilberto <>
2017-10-25 16:13:28 -0200

    Alterações para correção do tratamento e exibição de vínculos unidade X estabelecimentos/entidades.

--- MovJudModel/src/br/jus/tjsp/movjud/persistence/estruturajudiciaria/dao/EstabelecimentoPrisionalDAO.java
+++ MovJudModel/src/br/jus/tjsp/movjud/persistence/estruturajudiciaria/dao/EstabelecimentoPrisionalDAO.java
@@ -6,6 +6,8 @@ import br.jus.tjsp.movjud.persistence.base.dao.Paginacao;
 import br.jus.tjsp.movjud.persistence.entity.EstabelecimentoPrisional;
 import br.jus.tjsp.movjud.persistence.entity.Regiao;
 
+import br.jus.tjsp.movjud.persistence.entity.UnidadeEstabelecimentoPrisional;
+
 import java.util.List;
 
 import javax.ejb.Local;
@@ -17,4 +19,11 @@ public interface EstabelecimentoPrisionalDAO extends BaseDAO<EstabelecimentoPris
                                                                                    Paginacao paginacao);
     
     List<EstabelecimentoPrisional> listarEstabelecimentosPrisionaisOrdenadoPorNome(EstabelecimentoPrisional estabelecimentoPrisional);
+    
+    List<EstabelecimentoPrisional> listarEstabelecimentosPrisionaisSemUnidadeAtivoPorNome(EstabelecimentoPrisional estabelecimentoPrisional);
+    List<EstabelecimentoPrisional> listarEstabelecimentosPrisionaisSemUnidadeAtivoPorNome(EstabelecimentoPrisional estabelecimentoPrisional,
+                                                                                        Paginacao paginacao);
+    // <edilberto item 199>
+    UnidadeEstabelecimentoPrisional obterVinculoMaisRecenteComUnidade(EstabelecimentoPrisional estabelecimentoPrisional);
+    // </edilberto item 199>
 }
