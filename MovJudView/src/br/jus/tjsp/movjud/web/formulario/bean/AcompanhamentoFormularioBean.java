a21049479d8e44a214efb90ac761b7c5cfdd7da1
CDS <CDS@NTC_SP041.mshome.net>
2018-06-15 14:19:50 -0300

    Correcao do retorno da funcao de visibilidade

--- MovJudView/src/br/jus/tjsp/movjud/web/formulario/bean/AcompanhamentoFormularioBean.java
+++ MovJudView/src/br/jus/tjsp/movjud/web/formulario/bean/AcompanhamentoFormularioBean.java
@@ -2972,15 +2972,19 @@ public class AcompanhamentoFormularioBean extends BaseBean<FormularioDTO> {
             }
         }
         
+        boolean retorno = true;
+        
         for (Map.Entry<Long, Boolean> entry : tiposRegrasFomrulario.entrySet()) {
             if (campo.getTipoRegraDTO() == null) break;
             
             if (entry.getKey() == campo.getTipoRegraDTO().getId()) {
                 return !campo.getTipoRegraDTO().isInverterRegra();
+            } else {
+                retorno = false;
             }
         }
         
-        return true;
+        return retorno;
     }
 
     private FormularioDTO recuperarFormulario(FormularioDTO form) {
